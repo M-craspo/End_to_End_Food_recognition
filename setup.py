9a22fa54137ce66f14349817f8a871f96fb17e2e
M-craspo <elbahymahmoud206@gmail.com>
2025-01-18 16:28:49 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Foodrecognition',
+    version= '0.0.0',
+    author= 'Mahmoud Elbahy',
+    author_email= 'elbahymahmoud206@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
